dfdd12b3c1d0e98cb82ec8de65f15b4297915090
Ramiro Richmand <ramirorichmand@Ramiros-Coding-MacBook-Pro.local>
2023-02-20 14:11:56 +0000

    first push

---
+++ src/main/java/com/BookkeeperBackendProject/BookkeeperBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BookkeeperBackendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeeperBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeeperBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookkeeperBackendProject/BookkeeperBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookkeeperBackendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeeperBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
